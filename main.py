a7c490b914512e9bdc92d2e8532552e79c547419
LFerrin24 <825875@mystma.org>
2023-03-31 14:24:00 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    numA = 0
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            numA = numA + 1
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
